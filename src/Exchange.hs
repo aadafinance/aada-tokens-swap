5fb59345e225ac5bc1083309b548e02b0ba2bb0f
taada-finance <tomas@aada.finance>
2022-01-18 17:43:04 +0200

    Mainnet v1

--- src/Exchange.hs
+++ src/Exchange.hs
@@ -9,12 +9,11 @@
 {-# LANGUAGE TypeOperators       	#-}
 {-# LANGUAGE RecordWildCards     	#-}
 
--- This example is taken directly from cardano-api, written by Jordan Millar, IOHK
-
 module Exchange
   ( exchange
   , exchangeShortBs
   ) where
+
 import           Cardano.Api.Shelley (PlutusScript (..), PlutusScriptV1) 
 import           Codec.Serialise
 import qualified Data.ByteString.Lazy as LBS
@@ -23,9 +22,7 @@ import qualified Data.ByteString.Short as SBS
 import           Plutus.V1.Ledger.Contexts
 import qualified Plutus.V1.Ledger.Scripts as Plutus
 import           Plutus.V1.Ledger.Address
-import           Plutus.V1.Ledger.Time 
 import           Plutus.V1.Ledger.Scripts
-import           Plutus.V1.Ledger.Credential
 import           Plutus.V1.Ledger.Api
 import           Plutus.V1.Ledger.Value
 import qualified PlutusTx
@@ -36,28 +33,28 @@ import           Ledger.Typed.Scripts as Scripts
 
 data ContractInfo = ContractInfo
     { secretHash             :: !DatumHash
-    , oldAadaPolicyID        :: !CurrencySymbol	
+    , oldAadaPolicyID        :: !CurrencySymbol
     , newAadaPolicyID        :: !CurrencySymbol
     , aadaBurnScAddrH        :: !ValidatorHash
     , aadaEmergencyScAddrH   :: !ValidatorHash
     , oldTokenName           :: !TokenName
     , newTokenName           :: !TokenName
-    , aadaPkh                :: !PubKeyHas
+    , aadaPkh                :: !PubKeyHash
     } deriving Show
 
 contractInfo = ContractInfo
     { secretHash             = "23b2dc8595610221997288830831937455a3fe5fab6ce823a3a01252a9410d32"
-    , oldAadaPolicyID        = "2b16738ac8f9cd2f0c30c8c1aef9d429ae02f5300734a902c730bb97" --CONY
-    , newAadaPolicyID        = "17830caa477d5eac7a4b6c123d98fa232e6e921368b01b7cd0bdfce6" --AADA
+    , oldAadaPolicyID        = "6f69a35a962c154bab4f2de4d9e116043697c333ade763c2b46fce6e"
+    , newAadaPolicyID        = "8fef2d34078659493ce161a6c7fba4b56afefa8535296a5743f69587"
     , aadaBurnScAddrH        = "c0c671fba483641a71bb92d3a8b7c52c90bf1c01e2b83116ad7d4536"
-    , aadaEmergencyScAddrH   = "e8e5f8aa6b99363f39a7d8883652e257be3a7311c908da9ab8116bb5"
-    , oldTokenName           = "CONY"
+    , aadaEmergencyScAddrH   = "a87652f3ace13f0109e01c6d035a9d2d07e1c24f9d421169cffe54a7"
+    , oldTokenName           = "AADA"
     , newTokenName           = "AADA"
-    , aadaPkh                = "ff"
+    , aadaPkh                = "9e88ebcc7f4dcabf3333e88f4e863b3981d4df91cee5e8f254125d03"
     }
 
 {-# INLINABLE mkValidator #-}
-mkValidator :: ContractInfo -> ExchangeDatum -> () -> ScriptContext -> Bool
+mkValidator :: ContractInfo -> Integer -> Integer -> ScriptContext -> Bool
 mkValidator contractInfo@ContractInfo{..} _ _ ctx = 
     if isItEmergencyUnlock then traceIfFalse "Wrong emergency claim address" checkAadaOutEmergencyAddr
     else
@@ -85,7 +82,7 @@ mkValidator contractInfo@ContractInfo{..} _ _ ctx =
 
     checkAadaOutEmergencyAddr :: Bool
     checkAadaOutEmergencyAddr = any isTxToAadaEmergency txOuts
-    
+
     checkIfTxFromAada :: Bool
     checkIfTxFromAada = 
       traceIfFalse "Old tokens are not being sent to burn sc"                                  checkBurn   &&
@@ -107,66 +104,63 @@ mkValidator contractInfo@ContractInfo{..} _ _ ctx =
     oldAadaTotal :: Integer
     oldAadaTotal = aadaTotalAmount txOuts oldAadaPolicyID oldTokenName
 
-    -- change rate from 10 to 1000000 on mainnet
     sameAmounts :: Integer -> CurrencySymbol -> TokenName -> TxOut -> Bool
-    sameAmounts oldAadaAmount cs tn tx = oldAadaAmount == 10 * txOutToValueOfPolicy cs tn tx 
+    sameAmounts oldAadaAmount cs tn tx = oldAadaAmount * 1000000 == txOutToValueOfPolicy cs tn tx
 
-    findNewAadaTx :: Integer -> Maybe TxOut
-    findNewAadaTx i = find (sameAmounts oldAadaTotal newAadaPolicyID newTokenName) txOuts
+    findNewAadaTx :: Maybe TxOut
+    findNewAadaTx = find (sameAmounts oldAadaTotal newAadaPolicyID newTokenName) txOuts
 
     txSignedByReceiver :: TxOut -> Bool
     txSignedByReceiver tx = case toPubKeyHash $ txOutAddress tx of
-      Just pkh -> txSignedBy info pkh
-      Nothing  -> False
+      Just pkh -> traceIfFalse "txSignedBy info pkh is false" (txSignedBy info pkh) || traceIfFalse "txSignedBy info aadaPkh is false" (txSignedBy info aadaPkh)
+      Nothing  -> traceIfFalse "tx is not signed by receiver or aadaPkh" False
 
     checkSrcDst :: Bool
     checkSrcDst = case findNewAadaTx of 
-       Just tx -> txSignedByReceiver tx || txSignedBy info aadaPkh 
-       Nothing -> False
-
+       Just tx -> traceIfFalse "Found transaction but tx is not signed by receiver" (txSignedByReceiver tx)
+       Nothing -> traceIfFalse "newAadaTx not found" False
+ 
     isOldAadaToBeBurnt :: TxOut -> Bool
-    isOldAadaToBeBurnt tx =
-      if aadaFilter oldAadaPolicyID tx 
-        then
-	  case toValidatorHash $ txOutAddress tx of
-            Just    valh -> valh == aadaBurnScAddrH -- || valh == ownHash ctx
-            Nothing      -> False
-        else
-	    True
+    isOldAadaToBeBurnt tx = if aadaFilter oldAadaPolicyID tx then
+          case toValidatorHash $ txOutAddress tx of
+            Just    valh -> valh == aadaBurnScAddrH
+            Nothing      -> traceIfFalse "Found toValidator hash which passed aada filter" False
+          else
+            True
 
     checkBurn :: Bool
     checkBurn = all isOldAadaToBeBurnt txOuts
 
-    checkChange :: Bool
-    -- TODO
-    -- the rest of first filter must be filtered yet again by txSignedByReceiver
-    -- then check if all of the remaining transactions go back to this same smartcontract
-    checkChange = $ filter (aadaFilter newAadaPolicyID) txOuts
+    isTxRemainingAadaTx :: TxOut -> Bool
+    isTxRemainingAadaTx tx = case findNewAadaTx of
+        Just tx' -> tx /= tx'
+        Nothing  -> True
 
-{-
+    isTxGoingBack :: TxOut -> Bool
+    isTxGoingBack tx = case toValidatorHash $ txOutAddress tx of
+        Just valh -> valh == ownHash ctx
+        Nothing   -> False
+
+    checkChange :: Bool
+    checkChange = all isTxGoingBack $ filter (isTxRemainingAadaTx) $ filter (aadaFilter newAadaPolicyID) txOuts
+
 data Exchange
 instance Scripts.ValidatorTypes Exchange where
-    type instance DatumType Exchange = ExchangeDatum
-    type instance RedeemerType Exchange = ExchangeRedeemer
--}
-
-data Exchange
-instance Scripts.ValidatorTypes Exchange
+    type instance DatumType Exchange = Integer
+    type instance RedeemerType Exchange = Integer
 
 typedValidator :: Scripts.TypedValidator Exchange
 typedValidator = Scripts.mkTypedValidator @Exchange
     ($$(PlutusTx.compile [|| mkValidator ||]) `PlutusTx.applyCode` PlutusTx.liftCode contractInfo)
      $$(PlutusTx.compile [|| wrap ||])
   where
-    wrap = Scripts.wrapValidator
+    wrap = Scripts.wrapValidator @Integer @Integer
 
 validator :: Validator
 validator = Scripts.validatorScript  typedValidator
 
 PlutusTx.makeIsDataIndexed ''ContractInfo [('ContractInfo, 1)]
 PlutusTx.makeLift ''ContractInfo
---PlutusTx.makeIsDataIndexed ''ExchangeDatum [('ExchangeDatum, 0)]
---PlutusTx.makeIsDataIndexed ''ExchangeRedeemer [('ExchangeRedeemer, 0)]
 
 script :: Plutus.Script
 script = Plutus.unValidatorScript validator
